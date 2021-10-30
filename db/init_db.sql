adb9b163a60dbff9168c046ab7d62c2b02cf6e05
AleksaC <aleksacukovic1@gmail.com>
2021-10-30 15:47:34 +0200

    Update db initialization script and add project bootstrap script

--- db/init_db.sql
+++ db/init_db.sql
@@ -3,5 +3,17 @@ CREATE DATABASE directus;
 CREATE USER
     admin
 WITH
-    CREATEDB
     PASSWORD 'admin';
+
+\c directus;
+
+set role admin;
+
+CREATE TABLE people (
+    id SERIAL NOT NULL,
+    name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+
+    PRIMARY KEY (id)
+);
